7949ed4104c4092a322be198c3b17525d8d8c65e
DWraith23 <bwilson5924@gmail.com>
2026-02-14 18:31:06 -0500

    Probably fixed boss

--- WraithTests/rooms/wraith_rooms_8.gd
+++ WraithTests/rooms/wraith_rooms_8.gd
@@ -3,13 +3,12 @@ extends Node2D
 @onready var boss_marker : Marker2D = $"Enemies/Boss Spawn"
 
 @onready var boss : PackedScene = preload("res://GameObjects/Enemies/charmfly.tscn")
-@onready var key_on_death : DoorKey = preload("res://Resources/dead_boss_key.tres")
 
 func _ready() -> void:
     pass
     
 func spawn_boss() -> void:
-    if GameBase.get_singleton().keysCollected.has(key_on_death):
+    if GameBase.get_singleton().pickups.has("boss_exit_key"):
         return
     var boss_instance : Charmfly = MittyUtils.spawn_at_location(boss, boss_marker.global_position)
     boss_instance.connect("dead", open_door)
